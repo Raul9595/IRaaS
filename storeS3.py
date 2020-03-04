b05d14676f627ed56e34785deeabc6b3234c043e
Raul9595 <rahulsvarm@gmail.com>
2020-03-04 15:49:41 -0700

    Update storeS3.py
    Updated to upload all files from a directory

--- storeS3.py
+++ storeS3.py
@@ -1,17 +1,28 @@
 import boto3
+import os
 
-REGION_NAME = "us-east-1"
-s3 = boto3.resource('s3')
-s3BucketName = 'image-rec-512'
-path = 'BTS1.mp4'
-put_into_bucket = True
 
-for my_bucket_object in s3.Bucket(s3BucketName).objects.filter(Prefix='input/'):
-    if path[5:] in my_bucket_object.key:
-        put_into_bucket = False
-        break
+def uploadDirectory(path, s3, bucketname):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            put_into_bucket = True
 
-if put_into_bucket:
-    s3.Bucket(s3BucketName).upload_file(path, 'input/' + path[5:],
-                                        ExtraArgs={"ACL": "public-read", "ContentType": "video/h264"})
+            # Check if the file exists in the bucket already
+            for my_bucket_object in s3.Bucket(bucketname).objects.filter(Prefix='input/'):
+                if file in my_bucket_object.key:
+                    put_into_bucket = False
+                    break
 
+            # Put into bucket if the file does not exist in the s3 database
+            if put_into_bucket:
+                s3.Bucket(bucketname).upload_file(os.path.join(root, file), 'input/' + file,
+                                                    ExtraArgs={"ACL": "public-read", "ContentType": "video/h264"})
+
+
+if __name__ == '__main__':
+    s3 = boto3.resource('s3')
+    s3BucketName = 'image-rec-512'
+    # Local folder path
+    path = 'data'
+    # Upload all the files from the path
+    uploadDirectory(path, s3, s3BucketName)
