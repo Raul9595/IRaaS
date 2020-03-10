fb3cb230f4208c31961c3704ad208464e8b56c6d
Vivek Maskara <maskaravivek@gmail.com>
2020-03-10 00:48:25 -0700

    With start stop instance code

--- startInstances.py
+++ startInstances.py
@@ -1,5 +1,7 @@
 import boto3
 import json
+from botocore.exceptions import ClientError
+from utils import read_file
 
 BUCKET_NAME = "image-rec-512"
 CONFIG_FILE_KEY = "config/config.json"
@@ -7,7 +9,7 @@ BUCKET_INPUT_DIR = "input"
 BUCKET_OUTPUT_DIR = "output"
 
 
-def launch_instance(ec2, config):
+def launch_new_instance(ec2, config):
     # EC2 Instance Configuration details
     tag_specs = [{}]
     tag_specs[0]['ResourceType'] = 'instance'
@@ -28,8 +30,21 @@ def launch_instance(ec2, config):
 
     return instance_id, new_instance_resp
 
+def start_instance(ec2, instance_id):
+    try:
+        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
 
-def create_instances():
+def stop_instance(ec2, instance_id):
+    try:
+        response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
+
+def create_instances(ec2, ec2_config):
     # Queue instance which retrieves all the messages
     sqs = boto3.resource('sqs')
     queue_name = 'ImageRec'
@@ -57,17 +72,23 @@ def create_instances():
             break
         else:
             # Launch instances for each sqs message
-            s3 = boto3.client('s3')
-            result = s3.get_object(Bucket=BUCKET_NAME, Key=CONFIG_FILE_KEY)
-            ec2_config = json.loads(result["Body"].read().decode())
-            ec2 = boto3.client('ec2', region_name=ec2_config['region'])
-
-            result = launch_instance(ec2, ec2_config)
+            result = launch_new_instance(ec2, ec2_config)
             # Print the location of the file in the bucket from the message
             print(message_body[0].get('Records')[0].get('s3').get('object').get('key'))
-            print('Launched EC2 instance - '.format(result[0]))
+            print('Launched EC2 instance - {}'.format(result[0]))
+            stop_instance(ec2, result[0])
             queue.delete_messages(Entries=delete_messages)
 
+def get_config_from_file():
+    return json.loads(read_file('./config/config.json'))
+
+def get_config_from_s3():
+    s3 = boto3.client('s3')
+    result = s3.get_object(Bucket=BUCKET_NAME, Key=CONFIG_FILE_KEY)
+    ec2_config = json.loads(result["Body"].read().decode())
 
 if __name__ == '__main__':
-    create_instances()
+    ec2_config = get_config_from_file()
+    ec2 = boto3.client('ec2', region_name=ec2_config['region'])
+    create_instances(ec2, ec2_config)
+    stop_instance(ec2, 'i-091db2b47191de217')
