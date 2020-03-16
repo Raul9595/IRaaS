90ebd9c0c2697500297fc3e6e954d46dd80087a3
Raul9595 <rahulsvarm@gmail.com>
2020-03-15 18:38:01 -0700

    Update startInstances.py
    Updated start instances to execute the commands in a sequence and allow only ine unique message in the delete messages queue

--- startInstances.py
+++ startInstances.py
@@ -59,46 +59,61 @@ def start_instances(ec2_client, sqs_messages):
     # Start multiple instances depending on the number of messages in SQS queue
     ec2 = boto3.resource('ec2')
     thread = [0] * len(sqs_messages)
+    instance_thread_link = {}
+    sqs_messages_delete = []
+
     instances = ec2.instances.filter(
         Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
     i = 0
     for instance in instances:
         print(instance, instance.id, instance.public_dns_name)
+        instance_thread_link[i] = instance.id
         thread[i] = threading.Thread(
             target=thread_work, args=(ec2_client, i, instance.id,))
         thread[i].start()
-        i = i+1
+        i = i + 1
         print(i)
-        if (i == len(sqs_messages)):
+        if i == len(sqs_messages):
             break
-    delete_messages_from_sqs_queue(delete_messages)
+
+    for i in range(len(sqs_messages)):
+        sqs_messages_delete.append(
+            {item: sqs_messages[i][item] for item in sqs_messages[i] if not item.startswith('Body')})
+    delete_messages_from_sqs_queue(sqs_messages_delete)
+
 
 def delete_messages_from_sqs_queue(messages):
     # Delete messages from queue
     sqs = boto3.resource('sqs')
     queue_name = 'ImageRec'
     queue = sqs.get_queue_by_name(QueueName=queue_name)
-    queue.delete_messages(Entries=delete_messages) 
+    queue.delete_messages(Entries=messages)
 
-def get_messages_from_sqs_queue():
+
+def get_messages_from_sqs_queue(delete_messages):
     # Queue instance which retrieves all the messages
     sqs = boto3.resource('sqs')
     queue_name = 'ImageRec'
     max_queue_messages = 10
 
     queue = sqs.get_queue_by_name(QueueName=queue_name)
-    
-    sqs_messages = []
+
     for message in queue.receive_messages(MaxNumberOfMessages=max_queue_messages):
         body = json.loads(message.body)
         # Get the message only if the message is created by the s3 instance
         if body.get('Records')[0].get('eventSource') == 'aws:s3':
-            sqs_messages.append({
+            delete_messages.append({
                 'Id': message.message_id,
-                'ReceiptHandle': message.receipt_handle
+                'ReceiptHandle': message.receipt_handle,
+                'Body': body
             })
-    print('Returning {} messages from SQS'.format(len(sqs_messages)))
-    return sqs_messages
+    delete_messages = list({v['Id']: v for v in delete_messages}.values())
+    if len(delete_messages):
+        print('Returning {} message from SQS'.format(len(delete_messages)))
+    else:
+        print('Returning {} messages from SQS'.format(len(delete_messages)))
+    return delete_messages
+
 
 def thread_work(ec2_client, tid, instance_id):
     ec2 = boto3.resource('ec2')
@@ -116,28 +131,10 @@ def thread_work(ec2_client, tid, instance_id):
     print('SSH into the instance')
     ssh.connect(hostname=current_instance[0].public_ip_address,
                 username='ubuntu', pkey=privkey)
-    cd = '"cd " + "/home/ubuntu/darknet"'
-    stdin, stdout, stderr = ssh.exec_command(cd)
-    stdin.flush()
+
+    stdin, stdout, stderr = ssh.exec_command(
+        'Xvfb :1 & export DISPLAY=:1; cd /home/ubuntu/darknet; pwd; ./darknet detector demo cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights video12.h264 > output.txt;')
     data = stdout.read().splitlines()
-    for line in data:
-        x = line.decode()
-        print(x)
-
-    stdin, stdout1, stderr = ssh.exec_command('pwd')
-    stdin.flush()
-
-    data = stdout1.read().splitlines()
-    print(data)
-    for line in data:
-        x = line.decode()
-        print(x)
-
-    stdin, stdout1, stderr = ssh.exec_command(
-        './darknet detector demo cfg/coco.cfg cfg/yolov3-tiny.cfg yolov3-tiny.weights video.h264')
-    stdin.flush()
-
-    data = stdout1.read().splitlines()
     print(data)
     for line in data:
         x = line.decode()
@@ -145,21 +142,22 @@ def thread_work(ec2_client, tid, instance_id):
 
     ssh.close()
 
+
 def check_queue_and_launch_instances(ec2_client, ec2_config):
     # Get all the messages from queue and delete it once the instances are created for each message
+    delete_messages = []
+
     while True:
-        delete_messages = get_messages_from_sqs_queue()
+        delete_messages = get_messages_from_sqs_queue(delete_messages)
 
         # If there are no more messages in the queue, break
         if len(delete_messages) == 0:
             break
         else:
-            '''
             # Launch instances for each sqs message
-            queue.delete_messages(Entries=delete_messages)
-            '''
             print('Starting instances as the SQS has messages')
             start_instances(ec2_client, delete_messages)
+
 
 def get_config_from_file():
     # Load config from local file
@@ -170,11 +168,10 @@ def get_config_from_s3():
     # Load config from S3
     s3 = boto3.client('s3')
     result = s3.get_object(Bucket=BUCKET_NAME, Key=CONFIG_FILE_KEY)
-    ec2_config = json.loads(result["Body"].read().decode())
+    return json.loads(result["Body"].read().decode())
 
 
 if __name__ == '__main__':
     ec2_config = get_config_from_file()
     ec2_client = boto3.client('ec2', region_name=ec2_config['region'])
     check_queue_and_launch_instances(ec2_client, ec2_config)
-    #stop_instance(ec2, 'i-091db2b47191de217')
