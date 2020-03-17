50959d8efef06ce972821f025324234b6b811333
Raul9595 <rahulsvarm@gmail.com>
2020-03-17 00:52:35 -0700

    Added files that execute on pi

---
+++ piExecute.py
@@ -0,0 +1,85 @@
+import boto3
+import ast
+import json
+from utils import read_file
+import paramiko
+import os.path
+import sys
+
+BUCKET_NAME = "image-rec-512"
+CONFIG_S3_FILE_KEY = "config/config.json"
+CONFIG_LOCAL_FILE_KEY = "./config/config.json"
+COMMANDS_S3_FILE_KEY = "config/commands.txt"
+COMMANDS_LOCAL_FILE_KEY = "config/commands.txt"
+BUCKET_INPUT_DIR = "input"
+BUCKET_OUTPUT_DIR = "output"
+SQS_QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/694968717068/ImageRec"
+
+
+def delete_message(sqs, queue_url, receipt_handle):
+    print(queue_url)
+    sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+
+
+def process_video(message):
+    ssh = paramiko.SSHClient()
+    input_video = ast.literal_eval(message['Body']).get('Records')[0].get('s3').get('object').get('key').split('/')[1]
+
+    commands = get_from_local('commands')
+    commands = commands.replace("inputFile", input_video)
+    commands = commands.replace("outputFile", input_video + "_output.txt")
+    commands = commands.replace("exType", "pi")
+    print('\nCommannds ')
+    print(commands)
+
+    stdin, stdout, stderr = ssh.exec_command(commands)
+    data = stdout.read().splitlines()
+    print(data)
+    for line in data:
+        x = line.decode()
+        print(x)
+
+    ssh.close()
+
+
+def get_message(sqs):
+    response = sqs.receive_message(QueueUrl=SQS_QUEUE_URL)
+    if 'Messages' in response.keys():
+        return response['Messages'][0]
+
+
+def get_from_local(file):
+    # Load config from local file
+    if file == 'config':
+        return json.loads(read_file(CONFIG_LOCAL_FILE_KEY))
+    elif file == 'commands':
+        my_path = os.path.dirname(sys.argv[0])
+        path = os.path.join(my_path, COMMANDS_LOCAL_FILE_KEY)
+        print(path)
+        file = open(path)
+        return file.read()
+
+
+def get_from_s3(file):
+    # Load config from S3
+    s3 = boto3.client('s3')
+    if file == 'config':
+        result = s3.get_object(Bucket=BUCKET_NAME, Key=CONFIG_S3_FILE_KEY)
+        return json.loads(result["Body"].read().decode())
+    elif file == 'commands':
+        result = s3.get_object(Bucket=BUCKET_NAME, Key=COMMANDS_S3_FILE_KEY)
+        return result
+
+
+if __name__ == '__main__':
+    sqs = boto3.client('sqs')
+    try:
+        while True:
+            message = get_message(sqs)
+            print(message)
+            if message:
+                delete_message(sqs, SQS_QUEUE_URL, message['ReceiptHandle'])
+                process_video(message)
+
+    except KeyboardInterrupt:
+        pass
--- storeS3.py
+++ storeS3.py
@@ -3,23 +3,28 @@ import os
 
 
 def uploadDirectory(path, s3, bucketname):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if '.mp4' in file:
-                put_into_bucket = True
+    try:
+        while True:
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    if '.mp4' in file:
+                        put_into_bucket = True
 
-                # Check if the file exists in the bucket already
-                for my_bucket_object in s3.Bucket(bucketname).objects.filter(Prefix='input/'):
-                    if file in my_bucket_object.key:
-                        put_into_bucket = False
-                        print('{0} already exists in the database'.format(file))
-                        break
+                        # Check if the file exists in the bucket already
+                        for my_bucket_object in s3.Bucket(bucketname).objects.filter(Prefix='input/'):
+                            if file in my_bucket_object.key:
+                                put_into_bucket = False
+                                break
 
-                # Put into bucket if the file does not exist in the s3 database
-                if put_into_bucket:
-                    s3.Bucket(bucketname).upload_file(os.path.join(root, file), 'input/' + file,
-                                                        ExtraArgs={"ACL": "public-read", "ContentType": "video/h264"})
-                    print('{0} is inserted into the database'.format(file))
+                        # Put into bucket if the file does not exist in the s3 database
+                        if put_into_bucket:
+                            s3.Bucket(bucketname).upload_file(os.path.join(root, file), 'input/' + file,
+                                                              ExtraArgs={"ACL": "public-read",
+                                                                         "ContentType": "video/h264"})
+                            print('{0} is inserted into the database'.format(file))
+
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == '__main__':
