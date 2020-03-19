472eb862f60e17614860ee2226e0644c5e321ff6
Raul9595 <rahulsvarm@gmail.com>
2020-03-18 21:03:34 -0700

    Flag set while execution

--- piExecute.py
+++ piExecute.py
@@ -3,6 +3,8 @@ import ast
 import subprocess
 import os.path
 import sys
+import paramiko
+import json
 
 BUCKET_NAME = "image-rec-512"
 CONFIG_S3_FILE_KEY = "config/config.json"
@@ -24,7 +26,40 @@ def add_message(message):
     response = queue.send_message(MessageBody=message['Body'])
 
 
+def connect_to_instance(instance_id, val):
+    ec2_config = get_from_local('config')
+    ec2 = boto3.resource('ec2', region_name=ec2_config['region'])
+    instance = ec2.Instance(id=instance_id)
+    print('Waiting for instance {} come in running state'.format(instance_id))
+    instance.wait_until_running()
+    print('Instance {} is now in running state'.format(instance_id))
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    privkey = paramiko.RSAKey.from_private_key_file(
+        './config/image_rec_auth.pem')
+    current_instance = list(ec2.instances.filter(InstanceIds=[instance_id]))
+    print(current_instance[0].public_ip_address)
+    print('SSH into the instance')
+    ssh.connect(hostname=current_instance[0].public_ip_address,
+                username='ubuntu', pkey=privkey)
+
+    commands = "echo \"" + str(val) + "\" > /home/ubuntu/pi_status.txt;"
+
+    print('\nCommands ')
+    print(commands)
+
+    stdin, stdout, stderr = ssh.exec_command(commands)
+    data = stdout.read().splitlines()
+    print(data)
+    for line in data:
+        x = line.decode()
+        print(x)
+
+    ssh.close()
+
+
 def process_video(message):
+    connect_to_instance('i-01b3d9f2d287a0a1c', 1)
     input_video = ast.literal_eval(message['Body']).get('Records')[0].get('s3').get('object').get('key').split('/')[1]
 
     commands = get_from_local('commands')
@@ -37,11 +72,16 @@ def process_video(message):
     try:
         proc = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         res = proc.communicate()
-        print("error =", res[1])
+
+        if res[1] is not None:
+            print('Adding message back to queue')
+            add_message(message)
+        connect_to_instance('i-01b3d9f2d287a0a1c', 0)
 
     except subprocess.CalledProcessError:
         print('Adding message back to queue')
         add_message(message)
+        connect_to_instance('i-01b3d9f2d287a0a1c', 0)
 
 
 def get_message(sqs):
@@ -53,7 +93,9 @@ def get_message(sqs):
 
 def get_from_local(file):
     # Load config from local file
-    if file == 'commands':
+    if file == 'config':
+        return json.loads(open(CONFIG_LOCAL_FILE_KEY).read())
+    elif file == 'commands':
         my_path = os.path.dirname(sys.argv[0])
         path = os.path.join(my_path, COMMANDS_LOCAL_FILE_KEY)
         file = open(path)
@@ -63,7 +105,10 @@ def get_from_local(file):
 def get_from_s3(file):
     # Load config from S3
     s3 = boto3.client('s3')
-    if file == 'commands':
+    if file == 'config':
+        result = s3.get_object(Bucket=BUCKET_NAME, Key=CONFIG_S3_FILE_KEY)
+        return json.loads(result["Body"].read().decode())
+    elif file == 'commands':
         result = s3.get_object(Bucket=BUCKET_NAME, Key=COMMANDS_S3_FILE_KEY)
         return result
 
@@ -75,8 +120,8 @@ if __name__ == '__main__':
             message = get_message(sqs)
             if message and ast.literal_eval(message['Body']).get('Records') is not None and \
                     ast.literal_eval(message['Body']).get('Records')[0].get('eventSource') == 'aws:s3':
-                delete_message(sqs, SQS_QUEUE_URL, message['ReceiptHandle'])
+                # delete_message(sqs, SQS_QUEUE_URL, message['ReceiptHandle'])
                 process_video(message)
 
     except KeyboardInterrupt:
-        pass
+        connect_to_instance('i-01b3d9f2d287a0a1c', 0)
