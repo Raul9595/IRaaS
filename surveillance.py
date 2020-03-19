430b254987e8c5312daf03d39d89e13072468c13
Raul9595 <rahulsvarm@gmail.com>
2020-03-18 18:47:44 -0700

    Created survelillance.py
    Created survelillance.py which check for motion and creates a 5 second video based on that

---
+++ surveillance.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+'''
+SETUP:
+
+    -   -->     GND     -->     PIN6
+    +   -->     5V      -->     PIN4
+    S   -->     GPIO18  -->     PIN12
+
+'''
+
+import RPi.GPIO as GPIO
+import subprocess
+import time
+import sys
+
+sensor = 12
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor, GPIO.IN)
+
+on = 0
+off = 0
+flag = 0
+max = int(sys.argv[1])
+
+while flag < max+1:
+    i=GPIO.input(sensor)
+    if i == 0:
+        print "No intruders"
+        time.sleep(1)
+    elif i == 1:
+        print "Intruder detected"
+        flag+=1
+        subprocess.call(['raspivid','-o','../darknet/video'+str(flag)+'.h264'])
+        time.sleep(0.1)
+        
