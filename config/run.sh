b1b317b44b7296483418459c8071870e5a39c226
Vivek Maskara <maskaravivek@gmail.com>
2020-03-27 14:35:20 -0700

    with updated commands

--- config/run.sh
+++ config/run.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-# check if package is installed
-# install package if not installed
-installPackage() {
-    retval=0
-    echo "Installing package $1"
-    if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        echo "Package $1 is not installed"
-        if echo "yes" | sudo apt-get install $1; then
-            retval=1
-        fi
-    else
-        echo "Package $1 is already installed"
-        retval=1
-    fi
-    return "$retval"
-}
-
 Xvfb :1 & export DISPLAY=:1;
-
-#Install AWS CLI if required
-installPackage awscli;
 
 #Copy Input file to EC2
 aws s3 cp s3://image-rec-512/input/$1 /home/ubuntu/darknet;
@@ -31,8 +10,8 @@ cd /home/ubuntu/darknet;
 
 ./darknet detector demo cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights $1 > output.txt;
 
-rm push.py
-wget https://image-rec-512.s3.amazonaws.com/config/push.py;
+# rm push.py
+# wget https://image-rec-512.s3.amazonaws.com/config/push.py;
 
 python3 push.py
 
