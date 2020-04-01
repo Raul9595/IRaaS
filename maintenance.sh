524e0181f86157f20404aa9decb5a6ba0bb4ef35
Raul9595 <rahulsvarm@gmail.com>
2020-03-31 20:44:53 -0700

    Deleted unnecessary files

--- maintenance.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws s3 rm --recursive s3://image-rec-512/input/
-aws s3 rm --recursive s3://image-rec-512/output/
-
--- utils.py
+++
@@ -1,5 +0,0 @@
-def read_file(file_name):
-    file = open(file_name, 'r', encoding='utf-8')
-    file_content = file.read()
-    file.close()
-    return file_content
